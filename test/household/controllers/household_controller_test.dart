7fac7589958508c8ac0d2628ee8047f14c16905a
Robert Brunhage <robertbrunhage@gmail.com>
2021-12-06 08:36:52 +0100

    Add more tests for household controller

--- test/household/controllers/household_controller_test.dart
+++ test/household/controllers/household_controller_test.dart
@@ -2,6 +2,9 @@ import 'package:budget_together/authentication/models/user.dart';
 import 'package:budget_together/authentication/services/auth_service.dart';
 import 'package:budget_together/authentication/supabase/supabase_provider.dart';
 import 'package:budget_together/household/controllers/household_controller.dart';
+import 'package:budget_together/household/models/category/category.dart';
+import 'package:budget_together/household/models/expense/expense.dart';
+import 'package:budget_together/household/models/household/household.dart';
 import 'package:budget_together/household/services/household_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -49,5 +52,168 @@ void main() {
     expect(container.read(householdControllerProvider).selectedYear, DateTime.now().year);
     expect(container.read(householdControllerProvider).household, isA<AsyncLoading>());
     expect(container.read(householdControllerProvider).categories, isA<AsyncLoading>());
+  });
+
+  test('When setting selected year Then year is correct in state', () async {
+    // ASSERT
+    expect(container.read(householdControllerProvider).selectedYear, DateTime.now().year);
+    container.read(householdControllerProvider.notifier).setYear(2010);
+    expect(container.read(householdControllerProvider).selectedYear, 2010);
+  });
+
+  test('When setting selected month Then month is correct in state', () async {
+    // ASSERT
+    expect(container.read(householdControllerProvider).selectedMonth, DateTime.now().month);
+    container.read(householdControllerProvider.notifier).setMonth(4);
+    expect(container.read(householdControllerProvider).selectedMonth, 4);
+  });
+
+  test('When creating household successfully Then fetch household is called', () async {
+    // ASSEMBLE
+    when(() => mockedHouseholdService.createHousehold('1', 'testingHousehold'))
+        .thenAnswer((invocation) => Future.value(const Success(null)));
+    expect(container.read(householdControllerProvider).household, isA<AsyncLoading>());
+
+    // ACT
+    await container.read(householdControllerProvider.notifier).createHousehold('1', 'testingHousehold');
+
+    // ASSERT
+    verify(() => container.read(householdServiceProvider).createHousehold('1', 'testingHousehold')).called(1);
+  });
+
+  test('When fetching household successfully Then verify state is set', () async {
+    // ASSEMBLE
+    final household = Household(id: 1, creator: '', name: '', expenses: []);
+    when(() => mockedHouseholdService.fetchHousehold(any())).thenAnswer(
+      (invocation) => Future.value(
+        Success(
+          household,
+        ),
+      ),
+    );
+
+    expect(container.read(householdControllerProvider).household, isA<AsyncLoading>());
+
+    // ACT
+    await container.read(householdControllerProvider.notifier).fetchHousehold();
+
+    // ASSERT
+    expect(container.read(householdControllerProvider).household.value, household);
+  });
+
+  test('When fetching expenses successfully Then verify state is set', () async {
+    // ASSEMBLE
+    final expenses = [
+      Expense(
+        id: 1,
+        amount: 100,
+        category: Category(id: 1, name: ''),
+        user: User(id: '', name: ''),
+        date: DateTime.now(),
+      )
+    ];
+    final household = Household(id: 1, creator: '', name: '', expenses: []);
+    when(() => mockedHouseholdService.fetchHousehold(any())).thenAnswer(
+      (invocation) => Future.value(
+        Success(
+          household,
+        ),
+      ),
+    );
+    when(() => mockedHouseholdService.fetchExpenses(any(), any(), any())).thenAnswer(
+      (invocation) => Future.value(
+        Success(expenses),
+      ),
+    );
+
+    // ACT
+    await container.read(householdControllerProvider.notifier).fetchHousehold();
+    await container.read(householdControllerProvider.notifier).fetchExpenses();
+
+    // ASSERT
+    expect(container.read(householdControllerProvider).household.value!.expenses, expenses);
+  });
+
+  test('When creating expense successfully Then verify fetch expenses is called', () async {
+    // ASSEMBLE
+    final expenses = [
+      Expense(
+        id: 1,
+        amount: 100,
+        category: Category(id: 1, name: ''),
+        user: User(id: '', name: ''),
+        date: DateTime.now(),
+      )
+    ];
+    final newExpense = Expense(
+      id: 2,
+      amount: 100,
+      category: Category(id: 1, name: ''),
+      user: User(id: '', name: ''),
+      date: DateTime.now(),
+    );
+    final household = Household(id: 1, creator: '', name: '', expenses: []);
+    when(() => mockedHouseholdService.fetchHousehold(any())).thenAnswer(
+      (invocation) => Future.value(
+        Success(
+          household,
+        ),
+      ),
+    );
+    when(() => mockedHouseholdService.fetchExpenses(any(), any(), any())).thenAnswer(
+      (invocation) => Future.value(
+        Success(expenses),
+      ),
+    );
+    when(() => mockedHouseholdService.createExpense(any(), 100, Category(id: -1, name: ''), any(), any())).thenAnswer(
+      (invocation) => Future.value(
+        const Success(null),
+      ),
+    );
+
+    // ACT
+    await container.read(householdControllerProvider.notifier).fetchHousehold();
+    await container.read(householdControllerProvider.notifier).createExpense(100, DateTime.now());
+
+    // ASSERT
+    verify(() => container.read(householdServiceProvider).fetchExpenses(any(), any(), any())).called(1);
+  });
+
+  test('When deleting expense successfully Then verify delete and fetch is called', () async {
+    // ASSEMBLE
+    final expenses = [
+      Expense(
+        id: 1,
+        amount: 100,
+        category: Category(id: 1, name: ''),
+        user: User(id: '', name: ''),
+        date: DateTime.now(),
+      )
+    ];
+    final household = Household(id: 1, creator: '', name: '', expenses: []);
+    when(() => mockedHouseholdService.fetchHousehold(any())).thenAnswer(
+      (invocation) => Future.value(
+        Success(
+          household,
+        ),
+      ),
+    );
+    when(() => mockedHouseholdService.fetchExpenses(any(), any(), any())).thenAnswer(
+      (invocation) => Future.value(
+        Success(expenses),
+      ),
+    );
+    when(() => mockedHouseholdService.deleteExpense(any())).thenAnswer(
+      (invocation) => Future.value(
+        const Success(null),
+      ),
+    );
+
+    // ACT
+    await container.read(householdControllerProvider.notifier).deleteExpense(1);
+
+    // ASSERT
+    verify(() => container.read(householdServiceProvider).deleteExpense(any())).called(1);
+    verify(() => container.read(householdServiceProvider).fetchExpenses(any(), any(), any())).called(1);
   });
 }
